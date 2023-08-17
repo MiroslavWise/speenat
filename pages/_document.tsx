379fce56db8776a143bd517c3d8ebe5014d3c13e
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2023-08-17 22:55:40 +0300

    fix head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
         return (
                 <Html lang="kz">
-                        <Head />
+                        <Head>
+                                <meta name="viewport" content="initial-scale=1.0, user-scalable=no, maximum-scale=1" />
+                        </Head>
                         <body>
                                 <Main />
                                 <NextScript />
